6176f4d6012467c4a0553889542e1a62b07a20d1
minaal07 <130892773+minaal07@users.noreply.github.com>
2024-12-22 17:24:14 +0530

    Refactored Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,58 @@
-import "./App.css";
-import Sidebar from "./components/Sidebar";
+import React, { useEffect, useState } from "react";
+import DashboardIcon from "@mui/icons-material/Dashboard";
+import { Outlet } from "react-router-dom";
+import { AppProvider } from "@toolpad/core/react-router-dom";
+import ChatIcon from "@mui/icons-material/Chat";
+import MailIcon from "@mui/icons-material/Mail";
+import SpeakerNotesIcon from "@mui/icons-material/SpeakerNotes";
 
+const NAVIGATION = [
+    {
+        kind: "header",
+        title: "Company",
+    },
+    {
+        segment: "",
+        title: "Dashboard",
+        icon: <DashboardIcon />,
+    },
+    {
+        segment: "chats",
+        title: "chats",
+        icon: <ChatIcon />,
+    },
+    {
+        kind: "divider",
+    },
+    {
+        kind: "header",
+        title: "Utilities",
+    },
+    {
+        segment: "leave",
+        title: "Leave",
+        icon: <ChatIcon />,
+    },
+    {
+        segment: "resignation",
+        title: "Resignation",
+        icon: <ChatIcon />,
+    },
+    {
+        segment: "Notes",
+        title: "Notes",
+        icon: <ChatIcon />,
+    },
+];
 export default function App() {
     return (
-        <div className="App">
-            <Sidebar />
-        </div>
+        <AppProvider
+            navigation={NAVIGATION}
+            branding={{
+                title: "Employee-Management-System",
+            }}
+        >
+            <Outlet />
+        </AppProvider>
     );
-};
-
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,53 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { StyledEngineProvider } from "@mui/material/styles";
+import * as React from "react";
 import "./index.css";
-import App from "./App.jsx";
+import * as ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Layout from "./layouts/Layouts";
+import Dashboard from "./pages/Dashboard";
+import Chats from "./pages/Chats";
+import Notes from "./pages/Notes";
+import Leave from "./pages/Leave";
+import Resignation from "./pages/Resignation";
 
-createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <BrowserRouter>
-            <StyledEngineProvider injectFirst>
-                <App />
-            </StyledEngineProvider>
-        </BrowserRouter>
-    </StrictMode>
+const router = createBrowserRouter([
+    {
+        Component: App,
+        children: [
+            {
+                path: "/",
+                Component: Layout,
+                children: [
+                    {
+                        path: "/",
+                        Component: Dashboard,
+                    },
+                    {
+                        path: "/chats",
+                        Component: Chats,
+                    },
+                    {
+                        path: "/notes",
+                        Component: Notes,
+                    },
+
+                    {
+                        path: "/leave",
+                        Component: Leave,
+                    },
+                    {
+                        path: "/resignation",
+                        Component: Resignation,
+                    },
+                ],
+            },
+        ],
+    },
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
